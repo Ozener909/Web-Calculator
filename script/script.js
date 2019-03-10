c4215506b7b02acccc64e9242edb1f75045d980d
Vladislav <vlad_sitnikov@inbox.ru>
2019-03-10 15:47:27 +0700

    Написан базовая js разметка

---
+++ script/script.js
@@ -0,0 +1,22 @@
+var numbers = document.querySelectorAll(".number"),
+    operation = document.querySelectorAll(".operation"),
+    point = document.querySelectorAll(".point"),
+    clear = document.querySelectorAll(".clear");
+console.log(numbers);
+console.log(operation);
+console.log(point);
+console.log(clear);
+
+
+function number(){
+
+};
+function operation(){
+
+};
+function point(){
+
+};
+function Clear(){
+
+};
